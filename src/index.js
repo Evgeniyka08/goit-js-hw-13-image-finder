8d7e1afc60e66f79966e906f309fb72aa44c1434
Evgeniy <evgeniyka08@i.ua>
2020-05-25 22:30:31 +0300

    HW-13

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import './styles/styles.css';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import getApiRequest from './js/apiServise.js';
+import templateListImages from './templates/list-images-templaye.hbs';
+import * as basicLightbox from 'basiclightbox';
+const debounce = require('lodash.debounce');
+const userKey = '13157109-fcd6eded5baca4880d7f5c7a8';
+
+const refs = {
+  listOfImages: document.querySelector('.list__images'),
+  input: document.querySelector('input[name="query"]'),
+  button: document.querySelector('.button-loader'),
+  modalCard: document.querySelector('.list__images'),
+};
+
+let numberOfPage = 1;
+let keyWord;
+
+refs.input.addEventListener('input', debounce(handlerSearchingImages, 1000));
+refs.button.addEventListener('click', handlerNexPagesOfImages);
+refs.modalCard.addEventListener('click', openModalWindow);
+
+function handlerSearchingImages(event) {
+  if (event.data === '' || event.data === null) {
+    refs.button.classList.remove('visible');
+    return;
+  }
+  refs.button.classList.remove('visible');
+  refs.listOfImages.innerHTML = '';
+  numberOfPage = 1;
+  keyWord = event.target.value;
+  getApiRequest(keyWord, numberOfPage, userKey).then(data => {
+    const markup = templateListImages(data);
+    refs.listOfImages.insertAdjacentHTML('beforeend', markup);
+    refs.button.classList.add('visible');
+  });
+}
+
+function handlerNexPagesOfImages(event) {
+  event.preventDefault();
+  numberOfPage += 1;
+  const pageHeight =
+    document.documentElement.offsetHeight - refs.button.clientHeight;
+
+  getApiRequest(keyWord, numberOfPage, userKey).then(data => {
+    refs.listOfImages.insertAdjacentHTML('beforeend', templateListImages(data));
+    window.scrollTo({
+      top: pageHeight,
+      behavior: 'smooth',
+    });
+  });
+}
+
+function openModalWindow(event) {
+  const eventImg = event.target.srcset;
+  const instance = basicLightbox.create(`<img src= ${eventImg}>`);
+  if (event.target.tagName === 'IMG') {
+    instance.show();
+  }
+}
